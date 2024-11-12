c7d9cffebd908e0aecc9ee4cf3993d9d446708ef
Victor <ninjavigge@gmail.com>
2024-11-12 11:28:58 +0100

    spara

--- pizza-boden-app/src/Components/HeaderComps/HeaderComp.tsx
+++ pizza-boden-app/src/Components/HeaderComps/HeaderComp.tsx
@@ -3,7 +3,16 @@ import './HeaderComp.css'
 function HeaderComp() {
     return (
         <>
-            
+            <header className="navbar">
+                <h1 className="navbar-title">Pizzaboden</h1>
+                <ul className="navbar-list">
+                    <li className="navbar-item">Hem</li>
+                    <li className="navbar-item">Meny</li>
+                    <li className="navbar-item">Beställ</li>
+                    <li className="navbar-item">Om oss</li>
+                    <li className="navbar-item">Kontakta</li>
+                </ul>
+            </header>
         </>
     )
 }
