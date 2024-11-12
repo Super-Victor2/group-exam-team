85362e8b9af0fe0abf64059470b44a3160b0913e
Victor <ninjavigge@gmail.com>
2024-11-12 11:03:36 +0100

    spara

--- pizza-boden-app/src/App.tsx
+++ pizza-boden-app/src/App.tsx
@@ -2,12 +2,14 @@ import './App.css'
 import { Route, Routes } from 'react-router-dom'
 
 import HomePage from './Pages/HomePage/HomePage'
+import Header from './Components/HeaderComps/HeaderComp'
 
 function App() {
 
   return (
     <>
       <div className='app'>
+      <Header />
       <Routes>
           <Route path='/' element={<HomePage />} />
       </Routes>
---
+++ pizza-boden-app/src/Components/HeaderComps/HeaderComp.tsx
@@ -0,0 +1,11 @@
+import './HeaderComp.css'
+
+function HeaderComp() {
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default HeaderComp
